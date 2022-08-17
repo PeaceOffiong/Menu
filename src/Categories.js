3ef0846dd681e7174d639469dd23b64c514bd38a
peaceOffiong <peaceyben@gamil.com>
2022-08-17 10:04:47 -0700

    first commit

---
+++ src/Categories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Categories = ({ filterItems, categories }) => {
+  return <section className='btn-container'>
+    {categories.map((category, index) => {
+      return <button key= {index}
+        className='filter-btn'
+        onClick={() => filterItems(category)}>{category}
+    </button>
+    })}
+  </section >;
+};
+
+export default Categories;
